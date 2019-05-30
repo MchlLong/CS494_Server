ed623f122ab4128601fcbdcd9a845d0588c9aced
MchlLong <michael.long.code@gmail.com>
2019-05-29 22:58:49 -0700

    Add project files.

---
+++ CS494_Server/CS494_Server.py
@@ -0,0 +1,9 @@
+# Michael Long, Gennadii Sytov -- CS494 -- Server GUI Controller -- May 2019
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ CS494_Server/gui_controller.py
@@ -0,0 +1,1 @@
+# Michael Long, Gennadii Sytov -- CS494 -- Server GUI Controller -- May 2019
