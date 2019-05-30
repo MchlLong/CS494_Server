862d32ea781ee59bbcf0080eb946a7fb357fc6f5
Michael Long <michael.long.code@gmail.com>
2019-05-30 10:19:13 -0700

    Testing GUI, in wrong implementation

--- CS494_Server/CS494_Server.py
+++ CS494_Server/CS494_Server.py
@@ -1,8 +1,12 @@
 # Michael Long, Gennadii Sytov -- CS494 -- Server GUI Controller -- May 2019
 # Laptop Test
 
+import gui_controller
+
 def main():
-    pass
+    print('Drawing the GUI')
+    a = gui_controller.gui_controller()
+
 
 if __name__ == '__main__':
     main()
