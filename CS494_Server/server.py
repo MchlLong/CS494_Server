33297d13d6db8b9bf35b92568f1a6cf24a97d8ec
MchlLong <michael.long.code@gmail.com>
2019-05-31 17:15:09 -0700

    > Usernames are now unique > Multiple options added > Can list all users with /user

--- CS494_Server/server.py
+++ CS494_Server/server.py
@@ -18,51 +18,135 @@ class server_handler():
         self.server.bind(self.address)
         # Data Side
         self.users = {}
+        self.rooms = {} # Usage rooms{Room_Name:user_list}
+        self.rooms['General'] = []
+        self.room_list = []
+        self.room_list.append('General')
+        self.user_count = 0
         print("Server has initialized (server_handler is ok)")
 
     def connect(self, connection):
-        connection.send(bytes("Hello, please enter a username: ", "utf8"))
+
+        connected = True
+        # for to_print in self.users.keys():
+        #    print(to_print)
+
         try:
+            # Add user, get a username to place into data structure
+            connection.send(bytes("Hello, please enter a username: ", "utf8"))
             username = connection.recv(BUFFER).decode("utf8")
+            # First user, no possibility of duplicates
+            if (self.user_count == 0):
+                self.user_count += 1
+            else:
+                # test for duplicate usernames
+                flag_entered = False
+                flag_repeat = False
+                for check in self.users.keys():
+                    if check == username:
+                        flag_repeat = True
+                if flag_repeat == False:
+                    pass
+                else:
+                    while flag_repeat:
+                        connection.send(bytes("Username is taken, please enter a valid username: ", "utf8"))
+                        username = connection.recv(BUFFER).decode("utf8")
+                        flag_repeat = False
+                        for check in self.users.keys():
+                            if check == username:
+                                flag_repeat = True
+
+
+
+
+            # Once a username has been established, welcome and print a series of commands
             connection.send(bytes("Welcome " + username + " type /? for a list of commands", "utf8"))
+            # Assign User Info
+            self.users[username] = connection
+            print(self.users[username])
+
             try:
-                self.users[connection, username] = connection
-                while True:
+
+                while connected:
 
                     print(username + " is giving data")
 
                     data = connection.recv(BUFFER)
 
                     # Check for commands
+
+                    # List set of commands 
                     if data == bytes("/?", "utf8"):
-                        connection.send(bytes('Commands:\n/join to join a chat room\n/leave to leave a chat room\n/quit to exit',"utf8"))
+                        connection.send(bytes('Commands:\n/create to make a chat room\n/join to join a chat room\n/leave to leave a chat room\n/users to print each user',"utf8"))
+                        connection.send(bytes('/list to show each room\n/disconnect to exit',"utf8"))
+
+                    # Add a user to a room
                     elif data == bytes("/join", "utf8"):
                         print(username + " is going to join a room")
-                        # insert join code here
+                        flag = False
+                        room = 'Room_A' #parse text for room_id
+
+                        # Verify that the room exists
+                        for check in self.room_list():
+                            if check == room:
+                                flag = True
+
+                        # If room exists, add the connection to the room
+                        if flag == True:
+                            self.rooms[room].append(connection)
+                            for key, val in self.rooms.items():
+                                print(key)
+                            
+                        
+                        
+
+                    # Remove a user from a room
                     elif data == bytes("/leave", "utf8"):
                         print(username + " is going to leave a room")
                         # insert leave code here
-                    elif data == bytes("/quit", "utf8"):
-                        break
+
+                    # Send a list of usernames
+                    elif data == bytes("/users", "utf8"):
+                        for to_print in self.users.keys():
+                            connection.send(bytes(to_print, "utf8"))
+
+                    # Send a list of all rooms to the user
+                    elif data == bytes("/list", "utf8"):
+                        for to_send in self.room_list:
+                            connection.send(bytes(to_send, "utf8"))
+
+                    
+                    # Disconnect a user from a room
+                    elif data == bytes("/disconnect", "utf8"):
+                        print(username + " has disconnected")
+                        del self.users[username]
+                        connected = False
+                        connection.send(bytes("You have been disconnected from the server.", "utf8"))               
+                        connection.close
+                        return
+
+                    # Send a message to each user sharing a room with user
                     else:
                         # Echo to verify functionality
                         print(data)
                         connection.send(data)
 
-
-
-                connection.send(bytes("You have been disconnected from the server.", "utf8"))
-                del self.users[connection, username]
-                connection.close
+            # Client disconnects with a specified username
             except:
                 print(username + " has disconnected")
+                del self.users[username]
+                connection.close()
+
+        # Client disconnects without specifying a username
         except:
             print("A user has disconnected")
 
+    # Main Loop to maintain a connection between a server and client
     def main_loop(self):
+        online = True
         print("The server is listening for a new connection (main_loop has a listener)")
         self.server.listen(5)
-        while True:
+        while online:
             print("A connection has been found (main_loop will create a new thread)")
             connection, address = self.server.accept()
             start_new_thread(self.connect, (connection,))
---
+++ CS494_Server/storage.py
@@ -0,0 +1,2 @@
+# Michael Long, Gennadii Sytov -- CS494 -- Server Application -- Server Class
+
