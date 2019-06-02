6e4ca3407ee07f4e2529cbb0aa6c536f4fa309e4
MchlLong <michael.long.code@gmail.com>
2019-06-01 20:00:55 -0700

    > Modified /join and /leave to clarify errors with input > Added the ability to create new rooms

--- CS494_Server/server.py
+++ CS494_Server/server.py
@@ -20,21 +20,18 @@ class server_handler():
         self.users = {}
         self.rooms = {} # Usage rooms{Room_Name:user_list}
         self.rooms['General'] = []
-        self.room_list = []
-        self.room_list.append('General')
         self.user_count = 0
         print("Server has initialized (server_handler is ok)")
 
     def connect(self, connection):
 
         connected = True
-        # for to_print in self.users.keys():
-        #    print(to_print)
 
         try:
             # Add user, get a username to place into data structure
             connection.send(bytes("Hello, please enter a username: ", "utf8"))
             username = connection.recv(BUFFER).decode("utf8")
+
             # First user, no possibility of duplicates
             if (self.user_count == 0):
                 self.user_count += 1
@@ -61,6 +58,8 @@ class server_handler():
 
             # Once a username has been established, welcome and print a series of commands
             connection.send(bytes("Welcome " + username + " type /? for a list of commands", "utf8"))
+            self.rooms['General'].append(username)
+            connection.send(bytes("You have joined the room!", "utf8"))
             # Assign User Info
             self.users[username] = connection
             print(self.users[username])
@@ -91,41 +90,94 @@ class server_handler():
                         elif text[0] == "/join" and len(text) > 1:
                             print(username + " is going to join a room")
                             flag = False
-                            room = text[1] 
+                            flag_dup = True
+                            attempt_room = text[1] 
+                            print('Successfully assigned room')
 
                             # Verify that the room exists
-                            for check in self.room_list():
-                                if check == room:
+                            for check in self.rooms.keys():
+                                print("Scanning room_list")
+                                if check == attempt_room:
                                     flag = True
 
-                            # If room exists, add the connection to the room
+                            # Verify the user is not in the room in the room
                             if flag == True:
-                                self.rooms[room].append(connection)
-                                for key, val in self.rooms.items():
-                                    print(key)
-                            else:
+                                for check in self.rooms[text[1]]:
+                                    print("Looking for duplicates: " + check)
+                                    if check == username:
+                                        flag_dup = False
+
+                            # If room exists, add the connection to the room
+                            if flag == True and flag_dup == True:
+                                print("Successfully added to room")
+                                self.rooms[attempt_room].append(username)
+                                connection.send(bytes("You have joined the room!", "utf8"))
+                            # Room exists, but the user is already in the room
+                            elif flag == True and flag_dup == False:
+                                connection.send(bytes("Error -- you're already in the room", "utf8"))
+                            # Room doesn't exist
+                            else: 
                                 connection.send(bytes("Error -- room does not exist", "utf8"))
                         
-                        
-
                         # Remove a user from a room
-                        elif data == bytes("/leave", "utf8"):
+                        elif text[0] == "/leave":
                             print(username + " is going to leave a room")
-                            # insert leave code here
+                            flag = False
+                            flag_inroom = False
+                            attempt_room = text[1] 
+                            # Verify that the room exists
+                            for check in self.rooms.keys():
+                                print("Scanning room_list")
+                                if check == attempt_room:
+                                    flag = True
+
+                            # Verify the user is in the room
+                            if flag == True:
+                                for check in self.rooms[text[1]]:
+                                    print("Looking for users in room")
+                                    if check == username:
+                                        flag_inroom = True
+
+                            # Room exists and the user is in the room, leave the room
+                            if flag == True and flag_inroom == True:
+                                print("Successfully left room")
+                                for check in self.rooms[text[1]]:
+                                    print(check)
+                                self.rooms[attempt_room].remove(username)
+                                connection.send(bytes("You have left the room!", "utf8"))
+                            # Room exists, user is not in the room, error
+                            elif flag == True and flag_inroom == False:
+                                connection.send(bytes("Error -- you can't leave a room you're not in", "utf8"))
+                            # Room doesn't exist, error
+                            else:
+                                connection.send(bytes("Error -- room does not exist", "utf8"))
+
+                        # Create a new room
+                        elif text[0] == "/create":
+                            print(username + " is going to create a room")
+                            flag = False
+                            flag_roomexists = False
+                            # Verify that the room doesn't exist
+                            for check in self.rooms.keys():
+                                print("Scanning room_list")
+                                if check == text[1]:
+                                    flag = True
+                            if flag == False:
+                                print("Created a new room")
+                                self.rooms[text[1]] = []
 
                         # Send a list of usernames
-                        elif data == bytes("/users", "utf8"):
+                        elif text[0] == "/users":
                             for to_print in self.users.keys():
                                 connection.send(bytes(to_print, "utf8"))
 
                         # Send a list of all rooms to the user
-                        elif data == bytes("/list", "utf8"):
-                            for to_send in self.room_list:
+                        elif text[0] == "/list":
+                            for to_send in self.rooms.keys():
                                 connection.send(bytes(to_send, "utf8"))
 
-                    
                         # Disconnect a user from a room
-                        elif data == bytes("/disconnect", "utf8"):
+                        elif text[0] == "/disconnect":
                             print(username + " has disconnected")
                             del self.users[username]
                             connected = False
@@ -134,7 +186,7 @@ class server_handler():
                             connection.close
                             return
 
-                        # Send a message to each user sharing a room with user
+                        # No valid command was sent, error
                         else:
                             # Echo to verify functionality
                             print(data)
@@ -162,7 +214,7 @@ class server_handler():
         print("The server is listening for a new connection (main_loop has a listener)")
         self.server.listen(5)
         while online:
-            print("A connection has been found (main_loop will create a new thread)")
             connection, address = self.server.accept()
             start_new_thread(self.connect, (connection,))
+            print("A connection has been found (main_loop will create a new thread)")
         self.server.close()
